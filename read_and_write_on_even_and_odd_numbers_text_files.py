d9853b664e4acea395c3f55f7f7d2bb6892522e6
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 11:32:27 +0800

    Pseudocodes are Added

---
+++ read_and_write_on_even_and_odd_numbers_text_files.py
@@ -0,0 +1,6 @@
+# create a numbers.txt file
+# read from the numbers.txt file
+# add a for loop
+# separation of even from odd numbers
+# write even numbers in even.txt file
+# write odd numbers in odd.txt file
