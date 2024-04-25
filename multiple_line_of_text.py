bbf100a5bf533e1a3d2be59e80b8fa66f8929448
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 17:53:21 +0800

    Added - Pseudocodes and For Inputting Lines

---
+++ multiple_line_of_text.py
@@ -0,0 +1,7 @@
+# write multiple lines of text contents into a text file, named "mylife.txt"
+# the user will input his/her content
+
+proceed = True
+while proceed:
+    line = input("Enter your line here: ")
+    break_line = line+("\n")
