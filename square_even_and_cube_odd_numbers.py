ffc6a289ff65ab1c15e10c5f8a648d10c085ae84
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 20:35:28 +0800

    Reading the Content of integers.txt

--- square_even_and_cube_odd_numbers.py
+++ square_even_and_cube_odd_numbers.py
@@ -1,2 +1,6 @@
 # create txt file for the 20 integers, named integers.txt
 # read the content of integers.txt
+with open("integers.txt", "r") as integers_file:
+    for number in integers_file:
+        number = int(number)
+        
