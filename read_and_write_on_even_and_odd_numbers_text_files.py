5372007d94fc8190981758a92bfc4a734c15ccea
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 11:50:15 +0800

    Write Odd Numbers in odd.txt File

--- read_and_write_on_even_and_odd_numbers_text_files.py
+++ read_and_write_on_even_and_odd_numbers_text_files.py
@@ -8,4 +8,7 @@ with open("numbers.txt", "r") as integers_file:
             # write even numbers in even.txt file
             with open("even.txt", "a") as output_of_even_numbers:
                 output_of_even_numbers.write(number)
-# write odd numbers in odd.txt file
+        else:
+            # write odd numbers in odd.txt file
+            with open("odd.txt", "a") as output_of_odd_numbers:
+                output_of_odd_numbers.write(number)
