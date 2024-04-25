d3a3c28a1e08e23253afe7ab913845398bede8b2
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 16:30:27 +0800

    Simplifying txt File

--- name_and_gwa_of_student.py
+++ name_and_gwa_of_student.py
@@ -1,5 +1,7 @@
 # create a students_data.txt file
 # simplify your txt file for easy reading
+txt_file = (r"students_data.txt")
+def find_highest_gwa(txt_file):
 # read the content of the students_data.txt file
 # adding a for loop
 # add designs to your output from pyfiglet, colorama, and Fore
