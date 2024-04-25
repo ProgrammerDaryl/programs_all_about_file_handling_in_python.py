61b760cd663f48deaa2a500245caa53d06f19c2d
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 11:40:29 +0800

    Add a For Loop

--- read_and_write_on_even_and_odd_numbers_text_files.py
+++ read_and_write_on_even_and_odd_numbers_text_files.py
@@ -1,7 +1,8 @@
 # create a numbers.txt file
 # read from the numbers.txt file
 with open("numbers.txt", "r") as integers_file:
-# add a for loop
+    # add a for loop
+    for number in integers_file:
 # separation of even from odd numbers
 # write even numbers in even.txt file
 # write odd numbers in odd.txt file
