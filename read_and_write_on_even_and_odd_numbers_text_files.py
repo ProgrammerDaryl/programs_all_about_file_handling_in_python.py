0df9ba0fc8febe90336b963f2ec6f28257b1e983
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 22:53:32 +0800

    Fixed Some Issues

--- read_and_write_on_even_and_odd_numbers_text_files.py
+++ read_and_write_on_even_and_odd_numbers_text_files.py
@@ -1,14 +1,14 @@
 # create a numbers.txt file
 # read from the numbers.txt file
 with open("numbers.txt", "r") as integers_file:
-    # add a for loop
+# add a for loop
     for number in integers_file:
-        # separation of even from odd numbers
+# separation of even from odd numbers
         if int(number) % 2 == 0:
-            # write even numbers in even.txt file
+# write even numbers in even.txt file
             with open("even.txt", "a") as output_of_even_numbers:
                 output_of_even_numbers.write(number)
         else:
-            # write odd numbers in odd.txt file
+# write odd numbers in odd.txt file
             with open("odd.txt", "a") as output_of_odd_numbers:
                 output_of_odd_numbers.write(number)
