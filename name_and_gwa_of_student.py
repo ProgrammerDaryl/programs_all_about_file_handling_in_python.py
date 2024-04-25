4912271ffb1888067633f92e248b3a261f263e5a
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 16:23:31 +0800

    Pseudocodes are Added

---
+++ name_and_gwa_of_student.py
@@ -0,0 +1,5 @@
+# create a students_data.txt file
+# simplify your txt file for easy reading
+# read the content of the students_data.txt file
+# adding a for loop
+# add designs to your output from pyfiglet, colorama, and Fore
