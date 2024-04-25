3987e19d90cb6e532218e62714f6420cf9eec1b6
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 20:49:14 +0800

    Cubing the Odd Numbers

--- square_even_and_cube_odd_numbers.py
+++ square_even_and_cube_odd_numbers.py
@@ -5,5 +5,13 @@ with open("integers.txt", "r") as integers_file:
         number = int(number)
         # check if the integers are divisible by 2
         if number % 2 == 0:
-            #squared the even numbers
+            # squared the even numbers
             squared = number **2
+            with open("double.txt", "a") as output_of_squared_even_numbers:
+                output_of_squared_even_numbers.write(str(squared) + "\n")
+                # not divisible by 2, will be odd numbers
+        else:
+            #cubed the odd numbers
+            cubed = number **3
+            with open("triple.txt", "a") as output_of_cubed_odd_numbers:
+                output_of_cubed_odd_numbers.write(str(cubed) + "\n")
