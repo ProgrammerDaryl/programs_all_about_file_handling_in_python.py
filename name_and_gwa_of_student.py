c2837b1f49e428aaa4ad56b2d02b2b9033724e69
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 16:35:27 +0800

    Reading the Content of the students_data.txt File

--- name_and_gwa_of_student.py
+++ name_and_gwa_of_student.py
@@ -2,6 +2,11 @@
 # simplify your txt file for easy reading
 txt_file = (r"students_data.txt")
 def find_highest_gwa(txt_file):
-# read the content of the students_data.txt file
+    # read the content of the students_data.txt file
+    with open(txt_file, "r") as students_grades:
+        data = students_grades.read()
+        txt_data = eval(data)
+        highest_gwa = None
+        student_name_highest_gwa = None
 # adding a for loop
 # add designs to your output from pyfiglet, colorama, and Fore
