9472eed34eaf70611b443e9d87c31340d5130f2e
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 18:06:30 +0800

    For Showing the Content Inputted by the User

--- multiple_line_of_text.py
+++ multiple_line_of_text.py
@@ -5,3 +5,9 @@ proceed = True
 while proceed:
     line = input("Enter your line here: ")
     break_line = line+("\n")
+    # txt file to show content of the multiple lines
+    with open("mylife.txt", "a") as output_of_my_life_text:
+        output_of_my_life_text.write(break_line)
+        while proceed:
+            #ask the user if he/she still wants to continue the program
+            
