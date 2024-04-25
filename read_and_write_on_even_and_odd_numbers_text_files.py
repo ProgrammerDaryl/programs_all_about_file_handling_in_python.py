967e6c146471b05cb0d50197976022aba6450d0e
ProgrammerDaryl <ProgrammerDaryl>
2024-04-25 11:42:45 +0800

    Separation of Even From Odd Numbers

--- read_and_write_on_even_and_odd_numbers_text_files.py
+++ read_and_write_on_even_and_odd_numbers_text_files.py
@@ -3,6 +3,7 @@
 with open("numbers.txt", "r") as integers_file:
     # add a for loop
     for number in integers_file:
-# separation of even from odd numbers
+        # separation of even from odd numbers
+        if int(number) % 2 == 0:
 # write even numbers in even.txt file
 # write odd numbers in odd.txt file
